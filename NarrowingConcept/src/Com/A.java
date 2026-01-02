abd74d7dcd0b7d5299c65d5915fbf7ef0ac83832
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2026-01-02 19:00:08 +0530

    commit

---
+++ NarrowingConcept/src/Com/A.java
@@ -0,0 +1,11 @@
+package Com;
+
+public class A {
+public void m1(Object o) {
+	System.out.println("m1-Obj");
+}
+
+public void m1(String s) {
+	System.out.println("m1-String");
+}
+}
---
+++ NarrowingConcept/src/Com/Test.java
@@ -0,0 +1,11 @@
+package Com;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		A a=new A();
+		a.m1(null);
+	}
+
+}
